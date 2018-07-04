ad68bf4fe255b78bf9d94d72ce6ce6c6df808bcf
gurezo <akihiko.kigure@gmail.com>
2018-07-04 09:33:59 +0900

    matInput add.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatInputModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
