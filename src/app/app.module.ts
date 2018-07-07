31724b7f222c6ca4eb718d36a86a7a63b3a0eb36
gurezo <akihiko.kigure@gmail.com>
2018-07-07 22:30:18 +0900

    remove matInput.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  MatInputModule
-} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -12,9 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatInputModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
